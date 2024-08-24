43fa3b00752a7cf464609449c1858f12f926cba9
chheangg <balldremz@gmail.com>
2024-08-23 17:25:34 -0700

    Add Vehicle entity and link it to customer

--- src/main/java/com/twistercambodia/karasbackend/customer/entity/Customer.java
+++ src/main/java/com/twistercambodia/karasbackend/customer/entity/Customer.java
@@ -1,16 +1,66 @@
 package com.twistercambodia.karasbackend.customer.entity;
 
+import com.twistercambodia.karasbackend.vehicle.entity.Vehicle;
 import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Customer {
     @Id
     @GeneratedValue(strategy =  GenerationType.UUID)
-    public String id;
+    private String id;
 
     @Column(unique = true, nullable = true)
-    public String name;
+    private String name;
 
     @Column
-    public String note;
+    private String note;
+
+    @OneToMany(mappedBy = "vehicle")
+    private Set<Vehicle> vehicles;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Set<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(Set<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id: '%s'," +
+                        "name: '%s'," +
+                        "note: '%s'," +
+                        "vehicles: [%s]]",
+                id, name, note, vehicles
+        );
+    }
 }
---
+++ src/main/java/com/twistercambodia/karasbackend/vehicle/entity/Vehicle.java
@@ -0,0 +1,112 @@
+package com.twistercambodia.karasbackend.vehicle.entity;
+
+import com.twistercambodia.karasbackend.customer.entity.Customer;
+import jakarta.persistence.*;
+
+@Entity
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @Column
+    private String vinNo;
+
+    @Column
+    private String model;
+
+    @Column
+    private int mileage;
+
+    @Column
+    private String note;
+
+    @Column
+    private String plateNumber;
+
+    @Column
+    private String makeAndModel;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getVinNo() {
+        return vinNo;
+    }
+
+    public void setVinNo(String vinNo) {
+        this.vinNo = vinNo;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getMakeAndModel() {
+        return makeAndModel;
+    }
+
+    public void setMakeAndModel(String makeAndModel) {
+        this.makeAndModel = makeAndModel;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Vehicle[id: '%s'," +
+                        "customer: '%s'," +
+                        "vinNo: '%s'," +
+                        "model: '%s'," +
+                        "mileage: %d," +
+                        "note: '%s'," +
+                        "plateNumber: '%s'," +
+                        "makeAndModeL '%s']",
+                id, vinNo, model, mileage, note, plateNumber
+        );
+    }
+}
