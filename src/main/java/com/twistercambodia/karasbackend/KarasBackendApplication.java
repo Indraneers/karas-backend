2c5495d3270f13b40b66be0af5ef6d612f55630b
julianthant <julianthant@gmail.com>
2024-08-27 14:42:08 -0700

    removed CommandLineRunner

--- src/main/java/com/twistercambodia/karasbackend/KarasBackendApplication.java
+++ src/main/java/com/twistercambodia/karasbackend/KarasBackendApplication.java
@@ -8,20 +8,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KarasBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KarasBackendApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(CategoryRepository categoryRepo) {
-        return runner -> {
-            createCategory(categoryRepo);
-        };
-    }
-
-    private void createCategory(CategoryRepository categoryRepo) {
-
-        // create the student object
     }
 }
