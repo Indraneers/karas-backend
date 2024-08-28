89df24a3c090f9874fd4eea34e9e07c3a1911aae
Eric <ericdong277@gmail.com>
2024-08-27 23:25:21 -0700

    Created the Status Entity

--- src/main/java/com/twistercambodia/karasbackend/sale/entity/Status.java
+++ src/main/java/com/twistercambodia/karasbackend/sale/entity/Status.java
@@ -1,4 +1,19 @@
 package com.twistercambodia.karasbackend.sale.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "status")
 public class Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "status_id")
+    private Sale sale;
 }
