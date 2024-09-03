f554d469a92d7ee7bd7fa3e1e38b855485036fb0
chheangg <balldremz@gmail.com>
2024-09-02 21:15:56 -0700

    add customer creation test

--- src/main/java/com/twistercambodia/karasbackend/customer/service/CustomerService.java
+++ src/main/java/com/twistercambodia/karasbackend/customer/service/CustomerService.java
@@ -7,9 +7,7 @@ import com.twistercambodia.karasbackend.customer.repository.CustomerRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
---
+++ src/test/java/com/twistercambodia/karasbackend/CustomerControllerTests.java
@@ -0,0 +1,65 @@
+package com.twistercambodia.karasbackend;
+
+import com.twistercambodia.karasbackend.configuration.MockConfig;
+import com.twistercambodia.karasbackend.customer.controller.CustomerController;
+import com.twistercambodia.karasbackend.customer.dto.CustomerDto;
+import com.twistercambodia.karasbackend.customer.entity.Customer;
+import com.twistercambodia.karasbackend.customer.repository.CustomerRepository;
+import com.twistercambodia.karasbackend.customer.service.CustomerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {KarasBackendApplication.class})
+@WebAppConfiguration
+public class CustomerControllerTests {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    void createCustomer_ShouldReturnNewCustomer_Status200() throws Exception {
+        CustomerDto customerDto = new CustomerDto();
+
+        customerDto.setName("Car Person A");
+        customerDto.setNote("Give them a discount next time!");
+
+        this.mockMvc.perform(
+                post("/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"Car Person A\", \"note\":\"Give them a discount next time!\" }")
+        )
+                .andExpect(status().isOk())
+                .andExpect(
+                        MockMvcResultMatchers.jsonPath("$.name")
+                                .value((customerDto.getName()))
+                )
+                .andExpect(
+                        MockMvcResultMatchers.jsonPath("$.note")
+                                .value((customerDto.getNote()))
+                );
+    }
+}
