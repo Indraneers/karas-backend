30a02c5d032e254046ddb522d1edf5f803cb47b2
chheangg <balldremz@gmail.com>
2025-02-09 23:06:33 -0800

    fix migration mismatches

--- migrations/V0.0.2__create_app_config_phone_numbers.sql
+++ migrations/V0.0.2__create_app_config_phone_numbers.sql
@@ -1,5 +1,5 @@
 CREATE TABLE app_config_phone_numbers (
   app_config_id BIGINT NOT NULL,
-  phone_numbers VARCHAR(50) NOT NULL,
+  phone_number VARCHAR(50) NOT NULL,
   FOREIGN KEY (app_config_id) REFERENCES app_config(id) ON DELETE CASCADE
 );
---
+++ migrations/V0.0.3__alter_phone_number_field.sql
@@ -0,0 +1,2 @@
+ALTER TABLE app_config_phone_numbers
+    RENAME COLUMN phone_number TO phone_numbers;
